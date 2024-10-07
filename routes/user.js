cdad90e9547463fd6e5db9aae04732f06307d9c2
chef_dill <dillon.sareerat@gmail.com>
2024-10-07 12:04:04 -0500

    commit user routes

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 
-router.get("/user", () => console.log("GET user"));
+router.get("/", () => console.log("GET user"));
+router.get("/:userId", () => console.log("GET user by ID"));
+router.post("/", () => console.log("POST user"));
 
 module.exports = router;
