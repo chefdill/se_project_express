fa3417effce94f0d269bb9f4ea0c8431aeaf265a
chef_dill <dillon.sareerat@gmail.com>
2024-10-08 20:47:57 -0500

    "app crashed" error

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
+const { getUsers } = require("../controller/user");
 
-router.get("/", () => console.log("GET user"));
+router.get("/", getUsers);
 router.get("/:userId", () => console.log("GET user by ID"));
 router.post("/", () => console.log("POST user"));
 
