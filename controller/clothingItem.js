c3e9a759cf33e393e6417d75de3da6984db52f97
chef_dill <dillon.sareerat@gmail.com>
2024-11-20 14:57:37 -0600

    changed imageUrl / changed 200 to 400

--- controller/clothingItem.js
+++ controller/clothingItem.js
@@ -14,7 +14,7 @@ const createItem = (req, res) => {
     .catch((err) => {
       console.error(err);
       if (err.name === "ValidationError") {
-        return res.status(200).send({ message: err.message });
+        return res.status(400).send({ message: err.message });
       }
       if (err.name === "DocumentNotFoundError") {
         return res.status(404).send({ message: err.message });
