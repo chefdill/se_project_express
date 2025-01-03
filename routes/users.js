ec82b95ef7b37afa08ce3b5336cdaf83b81cbdca
chef_dill <dillon.sareerat@gmail.com>
2025-01-03 17:02:14 -0600

    deleted unnecessary comment

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const { getCurrentUser, updateProfile } = require("../controllers/users");
 const { auth } = require("../middlewares/auth");
-// const { getUsers, createUser, getUser } = require("../controllers/users");
 
 router.get("/me", auth, getCurrentUser);
 router.patch("/me", auth, updateProfile);
